ce2fdd5d127707dfcbc64a95ae6284e3831636a9
Kati <katyushka.zykova.2015@mail.ru>
2024-01-18 20:00:29 +0300

    Task 2

--- src/test/java/ru/netelogy/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netelogy/service/CashbackHackServiceTest.java
@@ -1,48 +1,49 @@
 package ru.netelogy.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ShouldTest1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
         int actual = service.remain(amount);
 
-        assertEquals(actual, 0);
+        assertEquals(0, actual);
     }
-    @org.testng.annotations.Test
+
+    @org.junit.Test
     public void ShouldTest1() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1;
 
         int actual = service.remain(amount);
 
-        assertEquals(actual, 999);
+        assertEquals(999, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ShouldTest999() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
 
         int actual = service.remain(amount);
 
-        assertEquals(actual, 1);
+        assertEquals(1, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ShouldTest500() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
 
         int actual = service.remain(amount);
 
-        assertEquals(actual, 500);
+        assertEquals(500, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ShouldTest0() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
@@ -52,14 +53,14 @@ public class CashbackHackServiceTest {
         assertEquals(actual, 1000);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ShouldTest1001() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
 
         int actual = service.remain(amount);
 
-        assertEquals(actual, 0);
+        assertEquals(0, actual);
     }
 
 }
