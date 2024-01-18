9722e8d0dccfed96f04c77bd4d4073ca4b14f0e2
Kati <katyushka.zykova.2015@mail.ru>
2024-01-18 19:34:50 +0300

    Task 1

---
+++ src/test/java/ru/netelogy/service/CashbackHackServiceTest.java
@@ -0,0 +1,65 @@
+package ru.netelogy.service;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void ShouldTest1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, 0);
+    }
+    @org.testng.annotations.Test
+    public void ShouldTest1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, 999);
+    }
+
+    @org.testng.annotations.Test
+    public void ShouldTest999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, 1);
+    }
+
+    @org.testng.annotations.Test
+    public void ShouldTest500() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, 500);
+    }
+
+    @org.testng.annotations.Test
+    public void ShouldTest0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, 1000);
+    }
+
+    @org.testng.annotations.Test
+    public void ShouldTest1001() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+
+        assertEquals(actual, 0);
+    }
+
+}
